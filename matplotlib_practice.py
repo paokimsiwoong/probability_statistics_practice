2a69050e67f2b7401a0fffd068fd8932b432be66
paokimsiwoong <pao.kim.si.woong@gmail.com>
2025-09-22 07:50:37 +0900

    2: add matplotlib_practice.py - practice matplotlib basics

---
+++ matplotlib_practice.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+def main():
+    print("Hello from probability-statistics-practice!")
+
+    print(f'numpy version : {np.__version__}') # version check
+    print(f'matplotlib version : {mpl.__version__}') # version check
+
+    fig = plt.figure()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
